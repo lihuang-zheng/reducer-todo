b09602b70cf3b2eacea8ebb5aee17b9711e7dbfb
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-17 15:42:29 -0600

    added todo component and the reducer.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+//import todo component
+import Todo from "./components/Todo"
+
 function App() {
   return (
     <div className="App">
-
+      <Todo />
     </div>
   );
 }
