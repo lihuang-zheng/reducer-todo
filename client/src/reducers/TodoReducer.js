7987e10b88d9d749f69a40cf550a3936a67fa6c5
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-17 16:25:00 -0600

    changed the naming of the reducer.

---
+++ client/src/reducers/TodoReducer.js
@@ -0,0 +1,14 @@
+const initialState = [
+    {
+        item: 'Learning Reducer',
+        completed: false,
+        id: 123123123
+    },
+    {
+        item: 'Learning Complex Reducer',
+        completed: false,
+        id: 234234234
+    }
+]
+
+const 
