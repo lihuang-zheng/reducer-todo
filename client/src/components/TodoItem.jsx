f86a181a299840df109a13a0840011248e13a3d2
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-17 16:04:58 -0600

    added the structure of the todo item structure.

---
+++ client/src/components/TodoItem.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+const ListItem = ({ item, completed, id, state, dispatch }) => {
+    return (
+        <div className={"listItem" + ((completed) ? " taskCompleted" : "")}
+            onClick={() => dispatch({ type: "TOGGLE_COMPLETION_STATE", payload: id })}
+        >
+            <p>
+                {item} {completed ? " (completed)" : ""}
+            </p>
+            <button
+                onClick={(event) => {
+                    event.stopPropagation()
+                    dispatch({ type: "DELETE_TASK", payload: id })
+                }}
+            >delete</button>
+        </div>
+    )
+}
+
+export default ListItem
